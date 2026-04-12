325b68bdf813d1bb4844647268b8593f9bc927e0
rcherivi <rishika.cherivirala@gmail.com>
2026-04-12 17:44:07 -0400

    deleted extra line

--- src/routes.py
+++ src/routes.py
@@ -83,7 +83,7 @@ USE_LLM = False
 
 #     return scored_matches[:10]
 
-def json_search():
+
 def json_search():
     query = request.args.get("title", "")
     skill = request.args.get("skill", "")
