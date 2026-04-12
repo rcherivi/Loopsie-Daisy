387a59c7c7361f666a52907b4b767238ade4e6c8
yzhao2433 <yinglin991@gmail.com>
2026-04-12 16:11:07 -0400

    Removed plot elements that are not needed for the app

--- src/svd.py
+++ src/svd.py
@@ -21,12 +21,12 @@ pattern_data = []
 N_COMPONENTS = 600
 
 import numpy as np
-# import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 
-# def plot_optimal_k(patterns, max_k=1000):
-#     docs = [f"{p.title or ''} {p.description or ''}" for p in patterns]
+""" def plot_optimal_k(patterns, max_k=1000):
+    docs = [f"{p.title or ''} {p.description or ''}" for p in patterns]
 
 #     vectorizer = TfidfVectorizer(stop_words="english", ngram_range=(1, 2), sublinear_tf=True, min_df=2)
 #     tfidf_matrix = vectorizer.fit_transform(docs)
@@ -62,9 +62,9 @@ from sklearn.decomposition import TruncatedSVD
 #     plt.savefig("optimal_k.png", dpi=150)
 #     plt.close()  # free memory
 
-#     for t in [0.5, 0.6, 0.7, 0.8, 0.9]:
-#         k = np.searchsorted(cumvar, t) + 1
-#         print(f"  {int(t*100)}% variance explained at k={k}")
+    for t in [0.5, 0.6, 0.7, 0.8, 0.9]:
+        k = np.searchsorted(cumvar, t) + 1
+        print(f"  {int(t*100)}% variance explained at k={k}") """
 
 def build_svd_matrix(patterns):
     global vectorizer, svd, lsa_matrix, pattern_data
